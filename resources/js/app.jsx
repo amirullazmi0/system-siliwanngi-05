22510ab0eea37e30706738f94a18a3fe64389253
Amirul Azmi <amirullazmi0@gmail.com>
2023-05-15 16:27:51 +0700

    first commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,22 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Siliwangi 05';
+
+createInertiaApp({
+    // title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - Siliwangi 05`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
